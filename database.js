cca4b607c02f9e2bc49c0503f0acf44a487bfff0
Aryasatya2356 <lenathea0605@gmail.com>
2025-10-31 11:05:40 +0700

    menambahkan keamanan di database.js (sqlite), server.js untuk aut dan middleware (authmiddleware)

--- database.js
+++ database.js
@@ -34,6 +34,16 @@ const db = new sqlite3.Database(DB_SOURCE, (err) => {
                 //db.run(insert, ["Christopher Nolan", 1970]);
             }
         });
+
+        db.run (`CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+            )`, (err) => {
+                if (err) {
+                    console.error("Gagal membuat tabel users:", err.message);
+                }
+            });
     }
 });
 
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const db = require('./database.js');
+const jwt = require ('jsonwebtoken');
+const bcrypt = require('bcrypt'); 
+const JWT_SECRET = process.env.JWT_SECRET;
+const authenticateToken = require('./middleware/authMiddleware');
 
 const app = express();
 const PORT = process.env.PORT || 3300;
@@ -40,7 +44,7 @@ app.get('/movies/:id', (req, res) => {
     })
 });
 
-app.post('/movies', (req, res) => {
+app.post('/movies', authenticateToken, (req, res) => {
     const {title, director, year} = req.body;
     if (!title || !director || !year){
         return res.status(400).json({error: 'title, director, year wajib diisi'});
@@ -55,7 +59,7 @@ app.post('/movies', (req, res) => {
     });
 });
 
-app.put('/movies/:id', (req,res) => {
+app.put('/movies/:id', authenticateToken, (req,res) => {
     const {title, director, year} = req.body;
 
     const sql = 'UPDATE movies SET title = ?, director = ?, year = ? WHERE id = ?';
@@ -71,7 +75,7 @@ app.put('/movies/:id', (req,res) => {
     });
 });
 
-app.delete('/movies/:id', (req, res) => {
+app.delete('/movies/:id', authenticateToken,  (req, res) => {
     const sql = 'DELETE FROM movies WHERE id = ?';
     db.run(sql, [req.params.id], function(err){
         if (err){
@@ -155,6 +159,63 @@ app.delete('/directors/:id', (req, res) => {
             return res.status(404).json({error: 'Sutradara tidak ditemukan'});
         }
         res.status(204).send();
+    });
+});
+
+// === AUTH ROUTES ===
+app.post('/auth/register', (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password || password.length < 6) {
+        return res.status (400).json ({ error: 'Username dan password (min 6 char) harus disini' });
+    }
+
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+            console.error("Error hashing:", err);
+            return res.status(500).json({ error: 'Gagal memproses pendaftaran' });
+        }
+
+        const sql = 'INSERT INTO users (username, password) VALUES(?, ?)';
+        const params = [username.toLowerCase(), hashedPassword];
+        db.run(sql, params, function(err) {
+            if (err) {
+                if (err.message.includes(`UNIQUE constraint`)) {
+                    return res.status (409).json({ error: 'Username sudah digunakan' });
+                }
+                console.error("Error inserting user:", err);
+                return res.status(500).json({ error: 'Gagal menyimpan pengguna' });
+            }
+            res.status(201).json({ message: 'Registrasi berha', userId: this.lastID});
+        });
+    });
+});
+
+app.post('/auth/login', (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Username dan password harus diisi' });
+    }
+
+    const sql = "SELECT * FROM users WHERE Username = ?";
+    db.get(sql, [username.toLowerCase()], (err, user) => {
+        if (err || !user) {
+            return res.status(401).json({ error: 'Kredensial tidak valid' });
+        }
+
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err|| !isMatch) {
+                return res.status(401).json({ error: 'Kredensial tidak valid' });
+            }
+            const payload = { user: { id: user.id, username: user.username} };
+
+            jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
+                if (err) {
+                    console.error("Error signing token:", err);
+                    return res.status(500).json({ error: 'Gagal membuat token' });
+                }
+                res.json({ message: 'Login berhasil', token: token});
+            });
+        });
     });
 });
 
